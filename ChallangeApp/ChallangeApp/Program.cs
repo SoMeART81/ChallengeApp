2986e2cf66c58882e7fc61be79deb6da805fd2d1
Artur Samek <Artur.Samek@microsoft.wsei.edu.pl>
2023-03-23 00:12:36 +0100

    Zadanie Dzień 4

--- ChallangeApp/ChallangeApp/Program.cs
+++ ChallangeApp/ChallangeApp/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string name1 = "Ewa";
+string sex1 = "Kobieta";
+int age1 = 33;
+
+
+if (name1 == "Ewa" && age1 == 33)
+{
+    Console.WriteLine(name1 + ", lat " + age1);
+}
+
+
